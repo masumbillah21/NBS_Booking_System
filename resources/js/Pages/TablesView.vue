db87fcaa7ff2ccd253f03cdac8d151e7bb2ea6c7
H. M. Masum Billah <mbillah21@gmail.com>
2024-05-21 14:00:26 +0600

    first commit

---
+++ resources/js/Pages/TablesView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import BreadcrumbDefault from '@/Components/Breadcrumbs/BreadcrumbDefault.vue'
+import TableOne from '@/Components/Tables/TableOne.vue'
+import TableTwo from '@/Components/Tables/TableTwo.vue'
+import TableThree from '@/Components/Tables/TableThree.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+
+const pageTitle = ref('Tables')
+</script>
+
+<template>
+  <DefaultLayout>
+    <!-- Breadcrumb Start -->
+    <BreadcrumbDefault :pageTitle="pageTitle" />
+    <!-- Breadcrumb End -->
+
+    <div class="flex flex-col gap-10">
+      <TableOne />
+      <TableTwo />
+      <TableThree />
+    </div>
+  </DefaultLayout>
+</template>
